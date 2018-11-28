3686531e7d31c697bf53098ebed8ce99d6898141
Dante <42275387+TJANGEL@users.noreply.github.com>
2018-11-28 11:10:32 -0500

    added js file

--- assets/filter.js
+++ assets/filter.js
@@ -13,9 +13,9 @@ function filterNames() {
         let a = li[i].getElementsByTagName('a')[0];
         
         if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
-
+            li[i].style.display = '';
         } else {
-            
+            li[i].style.display = 'none';
         }
     }
 }
